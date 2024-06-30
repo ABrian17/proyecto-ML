c42a1f9dac092948b4174e021c1bd6fd1d109d0d
ABrian17 <brian_elbiencheto@hotmail.com>
2024-06-29 21:41:28 -0300

    estoy harto

--- app.js
+++ app.js
@@ -11,13 +11,13 @@ let port = 3030 || process.env.PORT;
 
 app.get("/", (req, res) => {
     res.sendFile(path.resolve("views/home.html"));
-})
+});
 app.get("/login", (req, res) => {
     res.sendFile(path.resolve("views/login.html"));
-})
+});
 app.get("/register", (req, res) => {
     res.sendFile(path.resolve("views/register.html"));
-})
+});
 
 app.listen(port, (err) => {
     err ?
